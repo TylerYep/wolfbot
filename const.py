aae0104cc4be0d51732c635c0f23406eec5af0c1
Tyler Yep <tyep@stanford.edu>
2018-05-04 16:40:08 -0700

    commenting

--- const.py
+++ const.py
@@ -4,11 +4,12 @@ import logging
 ### Simulation Constants ###
 NUM_GAMES = 400
 
+
 ### Game Constants ###
 ROLES = ('Villager', 'Villager', 'Villager', 'Wolf', 'Wolf', 'Seer', 'Mason', 'Mason')
 ROLE_SET = set(ROLES)
 NUM_ROLES = len(ROLES)
-ROLE_COUNTS = dict(Counter(ROLES))
+ROLE_COUNTS = dict(Counter(ROLES)) # {'Villager': 3, 'Wolf': 2, ... }
 
 NUM_CENTER = 3
 NUM_PLAYERS = NUM_ROLES - NUM_CENTER
