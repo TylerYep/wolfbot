6d5810a2db4da8cda9099498175c3be5aa596c89
harry <harryshahai@gmail.com>
2018-05-24 23:02:46 -0700

    hi

--- const.py
+++ const.py
@@ -6,19 +6,19 @@ logger = logging.getLogger()
 
 ### Simulation Constants ###
 NUM_GAMES = 1
-FIXED_WOLF_INDEX = None
+FIXED_WOLF_INDEX = 3
 
 ### Game Constants ###
 
-ROLES = ('Villager', 'Villager', 'Villager', 'Wolf', 'Wolf', 'Seer',
-        'Mason', 'Mason', 'Robber', 'Drunk', 'Troublemaker', 'Insomniac')
-# ROLES = ('Villager', 'Villager', 'Villager', 'Seer', 'Wolf')
+#ROLES = ('Villager', 'Villager', 'Villager', 'Wolf', 'Wolf', 'Seer',
+#        'Mason', 'Mason', 'Robber', 'Drunk', 'Troublemaker', 'Insomniac')
+ROLES = ('Villager', 'Villager', 'Villager', 'Seer', 'Wolf')
 
 ROLE_SET = set(ROLES)
 NUM_ROLES = len(ROLES)
 ROLE_COUNTS = dict(Counter(ROLES)) # Dict of {'Villager': 3, 'Wolf': 2, ... }
 
-NUM_CENTER = 3
+NUM_CENTER = 0
 NUM_PLAYERS = NUM_ROLES - NUM_CENTER
 
 ROBBER_PRIORITY = 1
@@ -29,6 +29,7 @@ USE_AI_PLAYERS = False
 
 ### Logging Constants ###
 logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.WARNING)
 
 ''' DEBUG = Include all hidden messages '''
 ''' INFO = Regular gameplay '''
