e7408554386dd5e68d405f310bdd858416fdf57a
Tyler Yep <tyep@stanford.edu>
2019-09-22 23:29:07 -0700

    predictions and voting test stubs

--- unit_test/predictions_test.py
+++ unit_test/predictions_test.py
@@ -1,1 +1,55 @@
 ''' predictions_test.py '''
+from src import predictions, const
+
+class TestMakeRandomPrediction:
+    def test_random_prediction(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestMakeEvilPrediction:
+    def test_evil_prediction(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestMakeFastPrediction:
+    def test_fast_prediction(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestMakePrediction:
+    def test_make_prediction(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestGetBasicGuesses:
+    def test_basic_guesses(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestRecurseAssign:
+    def test_recurse_assign(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestGetSwitchDict:
+    def test_get_switch_dict(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestPrintGuesses:
+    def test_print_guesses(self, caplog, medium_game_roles):
+        ''' Correctly print and format roles. '''
+        const.ROLES = medium_game_roles
+        predictions.print_guesses(list(medium_game_roles))
+
+        captured = caplog.records[0].getMessage()
+        expected = '\n[Wolfbot] Role guesses: [Robber, Drunk, Wolf, Troublemaker, Seer]\n' + \
+                   '          Center cards: [Minion]\n'
+        assert captured == expected
--- unit_test/voting_test.py
+++ unit_test/voting_test.py
@@ -1,1 +1,37 @@
 ''' voting_test.py '''
+from src import voting, const
+
+class TestConsolidateResults:
+    def test_consolidate_results(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestIsPlayerEvil:
+    def test_is_player_evil(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestGetIndividualPreds:
+    def test_get_individual_preds(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestEvalFinalGuesses:
+    def test_eval_final_guesses(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestGetVotingResult:
+    def test_get_voting_result(self):
+        ''' Should initialize a SolverState. '''
+        pass
+
+
+class TestGetPlayerVote:
+    def test_get_player_vote(self):
+        ''' Should initialize a SolverState. '''
+        pass
